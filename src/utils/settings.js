5300714a6b281a6fdc74d9993e693a9d60789b66
haoqianzhang <haoqian.zhang@connect.ust.hk>
2019-02-08 16:39:08 +0800

    Update default setting
    Update default setting

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,11 +1,13 @@
 const SETTINGS = {
     chains: [
-        "19AZfrNgBh5sxo5eVytX3K3yQvucS5vc45", // for testing
-        "15mhzB7c6rbDrSPimePU9zWMo3US9sAZzL" // for testing
+        // "19AZfrNgBh5sxo5eVytX3K3yQvucS5vc45", // for testing
+        // "15mhzB7c6rbDrSPimePU9zWMo3US9sAZzL" // for testing
+        "1BWJrzQBjWJ1JTBRXYrWRAU9dYdJJ2GXDM", 
+        "1PdJ5CLZfqUKy39RBfSPcwGnXvbdFgBuEW"
     ],
     peers: [
         //"ws://10.89.58.116:32767" //, "ws://10.89.58.116:32769"
-        "ws://10.89.58.116:32767"
+        "ws://hk.infnote.com:32767"
     ]
 }
 
