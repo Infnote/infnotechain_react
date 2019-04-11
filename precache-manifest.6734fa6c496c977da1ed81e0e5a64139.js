b4a24d2eb4dd74707afe98857dd95c030c79b8d4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 05:21:54 +0000

    Deploy blockchain.infnote.com to github.com/Infnote/infnotechain_react.git:gh-pages

---
+++ precache-manifest.6734fa6c496c977da1ed81e0e5a64139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a2d4ebd87aff7af33e4",
+    "url": "/static/css/main.74eae6bb.chunk.css"
+  },
+  {
+    "revision": "9a2d4ebd87aff7af33e4",
+    "url": "/static/js/main.9a2d4ebd.chunk.js"
+  },
+  {
+    "revision": "6a2909763b2fb2d68098",
+    "url": "/static/js/1.6a290976.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3dd431aeaebd7df326af9554a4cca54e",
+    "url": "/index.html"
+  }
+];
